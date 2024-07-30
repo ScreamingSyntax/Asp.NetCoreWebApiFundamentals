9689388211b394ee33980adc44836acc072f7017
Aaryan Jha <whcloud91@gmail.com>
2024-07-30 23:02:03 +0545

    ..

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//
 builder.Services.AddDbContext<TestDbContext>(
     options=> options.UseSqlServer(builder.Configuration.GetConnectionString("TestConnectionString"))
     );
