41550854a5c09285dfe3f6eabe1ec0b0f68b958f
Aaryan Jha <whcloud91@gmail.com>
2024-07-30 22:59:57 +0545

    COntrollers with CRUD & DTOS

---
+++ Models/DTO/RegionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Test.Models.DTO
+{
+    public class RegionDto
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionImageUrl { get; set; }
+
+    }
+    
+}
